0083f5f87f54026959c0cefe06e94ea151a3c918
fetahokey <ouldkradda.fateh@gmail.com>
2021-10-01 22:53:09 +0100

    set scroll-behavior to smooth

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,12 @@ import './topbar.scss'
 export default function Topbar() {
     return (
         <div className="topbar" id="intro">
-            
+            <div className="topbarWarpper">
+                <div className="topbarLeft">
+                    <a href="#works" className="topbarLogo">WORKS</a>
+                </div>
+                <div className="topbarRight"></div>
+            </div>
         </div>
     )
 }
