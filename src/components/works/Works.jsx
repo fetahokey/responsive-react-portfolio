02ce5b7d9a0fd622e794063486b60f757fdc55fb
fetahokey <ouldkradda.fateh@gmail.com>
2021-10-02 15:29:39 +0100

    + slider animation to the works section

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,8 +1,45 @@
 import './works.scss'
+import { worksData } from '../../dummyData'
+import { useState } from 'react';
 
 export default function Works() {
+    const handleSlide = (direction) => {
+        direction === 'left'
+            ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : worksData.length - 1)
+            : setCurrentSlide(currentSlide < worksData.length - 1 ? currentSlide + 1 : 0);
+    }
+    const [currentSlide, setCurrentSlide] = useState(0);
+
     return (
         <div className="works" id="works">
+            <div className="slider" style={{ transform: `translateX(-${currentSlide * 100}vw)` }}>
+                {worksData.map((item) => (
+                    <div className="container">
+                        <div className="item">
+                            <div className="left">
+                                <div className="leftContainer">
+                                    <div className="imgContainer">
+                                        <img src={item.icon} alt="" />
+                                    </div>
+                                    <h2>{item.title}</h2>
+                                    <p>
+                                        {item.desc}
+                                    </p>
+                                    <span>Project</span>
+                                </div>
+                            </div>
+                            <div className="right">
+                                <img
+                                    src={item.img}
+                                    alt=""
+                                />
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <img src="assets/img/arrow.png" className="arrow left" onClick={() => { handleSlide('left') }}></img>
+            <img src="assets/img/arrow.png" className="arrow right" onClick={() => { handleSlide('right') }}></img>
         </div>
     )
 }
--- src/dummyData.js
+++ src/dummyData.js
@@ -187,3 +187,33 @@ export const featuredPortfolio = [
     },
   ];
   
+
+  export const worksData = [
+    {
+      id: "1",
+      icon: "./assets/img/mobile.png",
+      title: "Web Design",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      img:
+        "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+    },
+    {
+      id: "2",
+      icon: "./assets/img/globe.png",
+      title: "Mobile Application",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img:
+        "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+    },
+    {
+      id: "3",
+      icon: "./assets/img/writing.png",
+      title: "Branding",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img:
+        "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+    },
+  ];
