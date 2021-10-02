e647a478abd5029b724775a137d26dd2634959bb
fetahokey <ouldkradda.fateh@gmail.com>
2021-10-02 02:10:09 +0100

    install @mui/material @emotion/react @emotion/styled

--- commands.bash
+++ commands.bash
@@ -9,3 +9,8 @@ npm audit fix
 
 # Install the package @mui/icons-material
 npm i @mui/icons-material
+
+# These components use the MUI SvgIcon component to render the SVG path for each icon.
+# If you are not already using MUI in your project, you can add it with:
+# https://github.com/mui-org/material-ui/issues/27846#issuecomment-902448679
+npm install @mui/material @emotion/react @emotion/styled
