7b1bafd3732a0c8a51c4310195073dc17fb884ac
fetahokey <ouldkradda.fateh@gmail.com>
2021-10-02 04:28:23 +0100

    intro initial style & animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,21 @@ import './intro.scss'
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/img/man.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Assif okey .Jr</h1>
+                    <h3>Software  <span>Engineer</span> </h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/img/down.png" alt="" />
+                </a>
+            </div>
         </div>
     )
 }
