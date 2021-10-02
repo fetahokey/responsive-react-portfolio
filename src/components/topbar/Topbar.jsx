bc30969cc0164d49db8063d7998377a9c4023d0b
fetahokey <ouldkradda.fateh@gmail.com>
2021-10-02 02:11:03 +0100

    udpate tobar left side

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,3 +1,4 @@
+import { Mail, Person } from '@mui/icons-material'
 import './topbar.scss'
 
 export default function Topbar() {
@@ -6,8 +7,22 @@ export default function Topbar() {
             <div className="warpper">
                 <div className="left">
                     <a href="#works" className="logo">aSSif.</a>
+                    <div className="itemContainer">
+                        <Person className="icon" />
+                        <span>+213 6552 4755</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon" />
+                        <span>oulkassif@gmail.com</span>
+                    </div>
                 </div>
-                <div className="right"></div>
+                <div className="right">
+                    <div className="hamburger">
+                        <span className="line1"> </span>
+                        <span className="line2"> </span>
+                        <span className="line3"> </span>
+                    </div>
+                </div>
             </div>
         </div>
     )
