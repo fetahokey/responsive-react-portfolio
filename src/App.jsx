0cb0a58b2008291f2656de9766c44c7160d14e62
fetahokey <ouldkradda.fateh@gmail.com>
2021-10-02 03:27:42 +0100

    + sidebar menu & anmiation

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Testimonials from "./components/testimonials/Testimonials"
 import Contact from "./components/contact/Contact"
 import "./app.scss";
 import { useState } from "react"
+import Menu from "./components/menu/Menu"
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -14,6 +15,8 @@ function App() {
     <div className="app">
       <Topbar menuOpen={menuOpen}
         setMenuOpen={setMenuOpen} />
+        <Menu menuOpen={menuOpen}
+        setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro />
         <Portfolio />
