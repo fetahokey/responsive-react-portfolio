e0b7ab7e034c74124dc29b8a20463716f5a2f080
fetahokey <ouldkradda.fateh@gmail.com>
2021-10-03 08:55:04 +0100

    Testimonials component & UI

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,41 @@
 import './testimonials.scss'
-
+import {testimonialsData} from '../../dummyData.js'
+/*
+id
+name
+title
+img
+icon
+desc
+ */
 export default function Testimonials() {
     return (
         <div className="testimonials" id="testimonials">
-
+            <h1>Testimonials</h1>
+                <div className="container">
+            {testimonialsData.map((item)=>(
+                <div className={item.featured ? "card featured" : "card"}>
+                    <div className="top">
+                        <img src={"assets/img/right-arrow.png"} className="left"  />
+                        <img className="user" src={item.img} alt="" />
+                        <img className="right" src={item.icon} />                        
+                    </div>
+                    <div className="center">
+                        <p>
+                            {item.desc}
+                        </p>
+                    </div>
+                    <div className="bottom">
+                        <h3>
+                        {item.name}
+                        </h3>
+                        <h4>
+                        {item.title}
+                        </h4>
+                    </div>
+                </div>
+            ))}
+            </div>
         </div>
     )
 }
--- src/dummyData.js
+++ src/dummyData.js
@@ -217,3 +217,36 @@ export const featuredPortfolio = [
         "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
     },
   ];
+  export const testimonialsData = [
+    {
+      id: 1,
+      name: "Tom Durden",
+      title: "Senior Developer",
+      img:
+        "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/img/twitter.png",
+      desc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
+    },
+    {
+      id: 2,
+      name: "Alex Kalinski",
+      title: "Co-Founder of DELKA",
+      img:
+        "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/img/youtube.png",
+      desc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
+      featured: true,
+    },
+    {
+      id: 3,
+      name: "Martin Harold",
+      title: "CEO of ALBI",
+      img:
+        "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/img/linkedin.png",
+      desc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+    },
+  ];
